e546892132db79e24048898bb6cec39c9302b9bd
RakiRoad <quintor2@tcnj.edu>
2017-07-11 23:32:12 -0400

    Changed Login config
    now uses facebook to login and now displays username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import {Observable} from 'rxjs/Observable';
 
+var provider = new firebase.auth.FacebookAuthProvider();
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   user: Observable<firebase.User>;
   items: FirebaseListObservable<any[]>;
   msgVal: string = '';
@@ -26,17 +27,26 @@ export class AppComponent {
     this.user = this.afAuth.authState;
 
   }
-
+  
   login() {
-    this.afAuth.auth.signInAnonymously();
+    this.afAuth.auth.signInWithPopup(provider);
   }
 
   logout() {
       this.afAuth.auth.signOut();
   }
 
-  Send(desc: string) {
-      this.items.push({ message: desc});
+  Send(elMessage: string) {
+      this.items.push({ message: elMessage, name: firebase.auth().currentUser.displayName});
       this.msgVal = '';
   }
 }
+
+/*
+    this.afAuth.auth.signInWithPopup(provider).then(function(result) {
+      // The firebase.User instance:
+      var user = result.user;
+      // The Facebook firebase.auth.AuthCredential containing the Facebook
+      // access token:
+      var 
+*/
